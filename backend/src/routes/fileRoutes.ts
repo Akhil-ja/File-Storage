bdd11db61bbe6e3885513ae996a78f84e1d1331a
Akhil Jagadish <akhiljagadish124@gmail.com>
2025-08-24 21:38:52 +0530

    refactor(validation,backend): centralize file validation and add constants

--- backend/src/routes/fileRoutes.ts
+++ backend/src/routes/fileRoutes.ts
@@ -7,14 +7,29 @@ import {
 } from "../controllers/fileController";
 import { protect } from "../middleware/authMiddleware";
 import multer from "multer";
+import { allowedMimeTypes, maxFileSize } from "../utils/constants";
 
 const router = Router();
-const upload = multer({ storage: multer.memoryStorage() });
+
+const upload = multer({
+  storage: multer.memoryStorage(),
+  limits: {
+    fileSize: maxFileSize,
+  },
+  fileFilter: (req, file, cb) => {
+    if (allowedMimeTypes.includes(file.mimetype)) {
+      cb(null, true);
+    } else {
+      cb(new Error("Invalid file type."));
+    }
+  },
+});
 
 router
   .route("/")
   .post(protect, upload.single("file"), uploadFile)
   .get(protect, getFiles);
+
 router.route("/:id").get(protect, downloadFile).delete(protect, deleteFile);
 
 export default router;
