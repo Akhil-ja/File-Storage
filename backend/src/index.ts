80ad0a9f04999e77030dd349bc91c08c1a3aaffd
Akhil Jagadish <akhiljagadish124@gmail.com>
2025-08-24 15:24:12 +0530

    feat(backend): initial backend server setup

---
+++ backend/src/index.ts
@@ -0,0 +1,18 @@
+import express from "express";
+import dotenv from "dotenv";
+import path from "path";
+
+dotenv.config({ path: path.resolve(__dirname, "../.env") });
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Heloo");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
