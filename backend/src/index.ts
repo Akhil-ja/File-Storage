0cfa2636b0b56762b006ee5290ce7a343ffee86e
Akhil Jagadish <akhiljagadish124@gmail.com>
2025-08-24 15:27:10 +0530

    feat(backend): Add cors and cookie parser

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,6 +2,8 @@ import express from "express";
 import dotenv from "dotenv";
 import connectDB from "./config/db";
 import path from "path";
+import cors from "cors";
+import cookieParser from "cookie-parser";
 
 dotenv.config({ path: path.resolve(__dirname, "../.env") });
 
@@ -9,6 +11,14 @@ connectDB();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+
+const corsOptions = {
+  origin: process.env.CLIENT_ORIGIN,
+  credentials: true,
+};
+
+app.use(cors(corsOptions));
+app.use(cookieParser());
 
 app.use(express.json());
 
