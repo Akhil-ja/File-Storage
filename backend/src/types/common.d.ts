af20eb5aa53d89c38cfb1243aa791ab2ba3d0ebe
Akhil Jagadish <akhiljagadish124@gmail.com>
2025-08-24 15:34:48 +0530

    feat(backend): Add used defined Types

---
+++ backend/src/types/common.d.ts
@@ -0,0 +1,32 @@
+import mongoose from "mongoose";
+
+declare global {
+  namespace Express {
+    interface Request {
+      user?: IUser;
+    }
+  }
+}
+
+export interface IUser extends mongoose.Document {
+  email: string;
+  password?: string;
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+export interface IFile extends mongoose.Document {
+  fileName: string;
+  originalName: string;
+  s3Key: string;
+  size: number;
+  fileType: string;
+  uploadedBy: mongoose.Schema.Types.ObjectId;
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+export interface AuthTokens {
+  accessToken: string;
+  refreshToken: string;
+}
